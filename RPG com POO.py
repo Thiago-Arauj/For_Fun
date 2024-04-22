15f71b4bccc147c72004f9c6dec6aa2e3e8c9163
Thiago Araujo <161383143+Thiago-Arauj@users.noreply.github.com>
2024-04-22 11:02:29 -0300

    Classe player criada

--- RPG com POO.py
+++ RPG com POO.py
@@ -8,5 +8,32 @@ class Player:
         self.level = level
         self.inventory = inventory
 
-    def lutar (self):
-        return randint()
+    def atacar (self, classe):
+        rolagem = randint(1,20)
+        if classe['Atk Mod'] != 0:
+            atk_roll = rolagem + classe['Atk Mod']
+        else:
+            atk_roll = rolagem + classe['Spl Mod']
+
+        return atk_roll
+    
+    def testes(self):
+        return randint(1, 20)
+
+def selec_classe():
+
+    classe = 'Inicio'
+    
+    while classe != 'Mago' or classe != 'Lutador':
+
+        classe = input('Qual classe deseja ser? \n Mago \n Lutador').capitalize()
+
+        if classe == 'Mago':
+            atributos = {'Armadura': 10, 'Atk Mod': 0, 'Spl Mod': 2}
+        elif classe == 'Lutador':
+            atributos = {'Armadura': 13, 'Atk Mod': 2, 'Spl Mod': 0}
+        else:
+            print(f'As classes disponiveis são lutador e mago. \n Sua seleção: {classe} \n Por favor selecione uma classe válida')
+
+    return atributos
+
